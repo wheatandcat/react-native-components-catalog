49760491aedf16c58df012be0a8252606f78d85e
飯野陽平 <iinoyouhei@iinoyouheinoMacBook-puro.local>
2018-04-10 17:09:53 +0900

    init

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import StorybookUI from './storybook';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+//module.exports = __DEV__ ? StorybookUI : App;
+//module.exports = StorybookUI;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
